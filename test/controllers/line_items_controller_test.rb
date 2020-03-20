2c0fb7e07fa0cdcac883121002aeb31d79f74268
LamNNRuby <nguyenngoclam3298@gmail.com>
2020-03-20 17:31:23 +0700

    11.Task F F4

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class LineItemsControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -16,14 +16,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create line_item" do
-    assert_difference('LineItem.count') do
+    assert_difference("LineItem.count") do
       post line_items_url, params: { product_id: products(:ruby).id }
     end
 
     follow_redirect!
 
-    assert_select 'h2', 'Your Cart'
-    assert_select 'td', "Programming Ruby 1.9"
+    assert_select "h2", "Your Cart"
+    assert_select "td", "Programming Ruby 1.9"
   end
 
   test "should show line_item" do
@@ -42,10 +42,20 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy line_item" do
-    assert_difference('LineItem.count', -1) do
+    assert_difference("LineItem.count", -1) do
       delete line_item_url(@line_item)
     end
 
     assert_redirected_to line_items_url
+  end
+  test "should create line_item via ajax" do
+    assert_difference("LineItem.count") do
+      post line_items_url, params: { product_id: products(:ruby).id },
+                           xhr: true
+    end
+    assert_response :success
+    assert_select_jquery :html, "#cart" do
+      assert_select "tr#current_item td", /Programming Ruby 1.9/
+    end
   end
 end
